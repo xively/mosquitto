06c2facdff61b23eca248408a761173d1fd93135
Lourens Naudé <lourens@methodmissing.com>
2014-03-06 01:03:27 +0000

    Make copies of messages received in on_message callbacks - libmosquitto clobbers them post callback

--- ext/mosquitto/message.c
+++ ext/mosquitto/message.c
@@ -13,10 +13,25 @@ VALUE rb_mosquitto_message_alloc(const struct mosquitto_message *msg)
 {
     VALUE message;
     mosquitto_message_wrapper *wrapper = NULL;
+    int ret;
     message = Data_Make_Struct(rb_cMosquittoMessage, mosquitto_message_wrapper, 0, rb_mosquitto_free_message, wrapper);
-    wrapper->msg = (struct mosquitto_message *)msg;
-    rb_obj_call_init(message, 0, NULL);
-    return message;
+    wrapper->msg = ALLOC(struct mosquitto_message);
+    ret = mosquitto_message_copy(wrapper->msg, msg);
+    switch (ret) {
+        case MOSQ_ERR_INVAL:
+            xfree(wrapper->msg);
+            xfree(wrapper);
+            MosquittoError("invalid input params");
+            break;
+        case MOSQ_ERR_NOMEM:
+            xfree(wrapper->msg);
+            xfree(wrapper);
+            rb_memerror();
+            break;
+        default:
+            rb_obj_call_init(message, 0, NULL);
+            return message;
+    }
 }
 
 VALUE rb_mosquitto_message_mid(VALUE obj)
@@ -51,24 +66,6 @@ VALUE rb_mosquitto_message_length(VALUE obj)
     return INT2NUM(msg->payloadlen);
 }
 
-VALUE rb_mosquitto_message_copy(VALUE obj)
-{
-    struct mosquitto_message *msg = NULL;
-    int ret;
-    MosquittoGetMessage(obj);
-    ret = mosquitto_message_copy(msg, (const struct mosquitto_message *)message->msg);
-    switch (ret) {
-        case MOSQ_ERR_INVAL:
-            MosquittoError("invalid input params");
-            break;
-        case MOSQ_ERR_NOMEM:
-            rb_memerror();
-            break;
-        default:
-            return rb_mosquitto_message_alloc((const struct mosquitto_message *)msg);
-    }
-}
-
 VALUE rb_mosquitto_message_qos(VALUE obj)
 {
     struct mosquitto_message *msg;
@@ -93,7 +90,6 @@ void _init_rb_mosquitto_message()
     rb_define_method(rb_cMosquittoMessage, "topic", rb_mosquitto_message_topic, 0);
     rb_define_method(rb_cMosquittoMessage, "to_s", rb_mosquitto_message_to_s, 0);
     rb_define_method(rb_cMosquittoMessage, "length", rb_mosquitto_message_length, 0);
-    rb_define_method(rb_cMosquittoMessage, "copy", rb_mosquitto_message_copy, 0);
     rb_define_method(rb_cMosquittoMessage, "qos", rb_mosquitto_message_qos, 0);
     rb_define_method(rb_cMosquittoMessage, "retain?", rb_mosquitto_message_retain_p, 0);
 }
