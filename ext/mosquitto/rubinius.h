8dc2a211378365e979d52f488941f244fb6a6203
Lourens Naudé <lourens@methodmissing.com>
2014-03-08 00:29:48 +0000

    Let 1.8 and Rubinius users know that only 1.9 and 2.x is supported

--- ext/mosquitto/rubinius.h
+++ ext/mosquitto/rubinius.h
@@ -1,6 +1,6 @@
 #ifndef MOSQUITTO_RUBINIUS_H
 #define MOSQUITTO_RUBINIUS_H
 
-#error Unsupported Ruby platform - this extension only works with Ruby 1.9 and Ruby 2
+#error Unsupported Ruby platform - this extension only works with Ruby 1.9 and Ruby 2.x
 
 #endif
--- ext/mosquitto/ruby18.h
+++ ext/mosquitto/ruby18.h
@@ -1,6 +1,6 @@
 #ifndef MOSQUITTO_RUBY18_H
 #define MOSQUITTO_RUBY18_H
 
-#error Unsupported Ruby platform - this extension only works with Ruby 1.9, Ruby 2 and Rubinius
+#error Unsupported Ruby platform - this extension only works with Ruby 1.9 and Ruby 2.x
 
 #endif
