70e41805e082329a7aaaf9563a379cab953b291e
Lourens Naudé <lourens@methodmissing.com>
2014-03-13 21:52:56 +0000

    Fix failing threads pub sub test

--- test/test_threads.rb
+++ test/test_threads.rb
@@ -27,38 +27,43 @@ class TestThreads < MosquittoTestCase
     threads = []
     published = 0
     messages = []
+    publisher, subscriber = nil
     threads << Thread.new do
+      subscriber = Mosquitto::Client.new
+      subscriber.loop_start
+      subscriber.logger = Logger.new(STDOUT)
+      subscriber.on_message do |msg|
+        messages << msg.to_s
+      end
+      subscriber.on_connect do |rc|
+        subscriber.subscribe(nil, "test_pub_sub", Mosquitto::AT_MOST_ONCE)
+      end
+      assert subscriber.connect(TEST_HOST, TEST_PORT, 10)
+      sleep 3
+      subscriber.loop_stop(true)
+    end
+
+    threads << Thread.new do
+      # let the subscription messages bubble through first
+      sleep 0.5
       publisher = Mosquitto::Client.new
       publisher.loop_start
+      publisher.logger = Logger.new(STDOUT)
+      publisher.on_publish do |mid|
+        published += 1
+      end
       publisher.on_connect do |rc|
         ('a'..'z').to_a.each do |message|
           publisher.publish(nil, "test_pub_sub", message, Mosquitto::AT_MOST_ONCE, true)
         end
       end
-      publisher.on_publish do |mid|
-        published += 1
-      end
       assert publisher.connect(TEST_HOST, TEST_PORT, 10)
-      sleep 2
+      sleep 3
       assert_equal published, 26
       publisher.loop_stop(true)
     end
 
-    threads << Thread.new do
-      subscriber = Mosquitto::Client.new
-      subscriber.loop_start
-      subscriber.on_connect do |rc|
-        subscriber.subscribe(nil, "test_pub_sub", Mosquitto::AT_MOST_ONCE)
-      end
-      subscriber.on_message do |msg|
-        messages << msg.to_s
-      end
-      assert subscriber.connect(TEST_HOST, TEST_PORT, 10)
-      sleep 2
-      subscriber.loop_stop(true)
-    end
-
-    threads.each(&:join)
+    threads.each{|t| t.join(3) }
     messages.uniq!
     messages.sort!
     assert_equal ('a'..'z').to_a, messages
