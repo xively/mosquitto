862e13838bf5e11af6041a47736ed9ba1d69a578
Lourens Naudé <lourens@methodmissing.com>
2014-03-14 03:11:31 +0000

    Word, bump test timeouts to 15s

--- test/helper.rb
+++ test/helper.rb
@@ -10,11 +10,11 @@ Thread.abort_on_exception = true
 
 class Mosquitto::Client
 if RUBY_VERSION.split(".").first == '2'
-  def wait_readable(timeout = 10)
+  def wait_readable(timeout = 15)
     IO.for_fd(socket).wait_readable(timeout)
   end
 else
-  def wait_readable(timeout = 10)
+  def wait_readable(timeout = 15)
     IO.for_fd(socket).wait(timeout)
   end
 end
