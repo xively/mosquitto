243438b544654a2400b1bb1a30aa11a06f327ee0
Lourens Naudé <lourens@methodmissing.com>
2014-03-25 18:55:02 +0000

    Let Client#wait_readable respect new socket connect || disconnect semantics of 1.3.1

--- lib/mosquitto/client.rb
+++ lib/mosquitto/client.rb
@@ -8,11 +8,21 @@ class Mosquitto::Client
 
   if RUBY_VERSION.split(".").first == '2'
     def wait_readable(timeout = 15)
+      retries ||= 0
       IO.for_fd(socket).wait_readable(timeout)
+    rescue Errno::EBADF
+      retries += 1
+      sleep 0.5
+      raise if retries > 4
     end
   else
     def wait_readable(timeout = 15)
+      retries ||= 0
       IO.for_fd(socket).wait(timeout)
+    rescue Errno::EBADF
+      retries += 1
+      sleep 0.5
+      raise if retries > 4
     end
   end
 end
